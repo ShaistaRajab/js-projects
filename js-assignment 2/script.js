a2468ef20b741595281e66d3b017a40a28a70631
ShaistaRajab <yasinraza95@gmail.com>
2023-04-19 00:04:39 +0500

    Make js assignment 1

---
+++ js-assignment 2/script.js
@@ -0,0 +1,118 @@
+// TASK 1:
+
+
+// var zakat_percentage = 0.025
+// var user_asset = + prompt ("Write your Assets Value");
+// var result = zakat_percentage*user_asset;
+// alert("Your Zakat " + result);
+
+
+// TASK 2:
+
+// var fMembers = +prompt("Please Enter your Family members");
+
+// var operation = +prompt("Enter Method You Want To Perform (1)Wheat (2)Raisin (3)Dates (4) Barley");
+// var Wheat = 250*fMembers;
+// alert(Wheat);
+// var Raisin = 2800*fMembers;
+// alert(Raisin);
+// var Dates = 2100*fMembers;
+// alert(Dates);
+// var Barley = 250*fMembers;
+// alert(Barley);
+ 
+
+// TASK 3:
+
+// var secretNum = 9
+// var userInput= +prompt("Guess The Number Between 1 till 10");
+// if(secretNum == userInput){
+//     console.log("Congratulations ! you guessed the secret number");
+// }
+// else if(secretNum < userInput)
+// console.log("Your value is low guess again");
+// else if(secretNum > userInput)
+// console.log("Your value is high guess again");
+
+// else{
+// console.log("Guess Again");
+
+// }
+
+
+// TASK 4:
+
+// var uNAME = prompt("Please Enter Your Name");
+// var capitalize = uNAME.slice(0,1).toUpperCase() + uNAME.slice(1).toLowerCase();
+// console.log(capitalize);
+// // Task 5:
+
+
+// var contactNames = []
+// var contactNumbers = []
+// for (var i = 0; i < 3; i++){
+// var userInput = prompt("Please Enter Your Contact Name");
+// var uInput = +prompt("Please Enter Your Contact Number");
+// contactNames.push(userInput);
+// contactNumbers.push(uInput);
+// console.log(`Contact Name: ${contactNames[i]}, Contact Number: ${contactNumbers[i]}`)};
+
+
+// TASK 6:
+
+// var products =[choclate , biscuits , buntees , chips];
+// var uInput = prompt("Select one option from products");
+
+
+// var products = ["choclates" , "bantees" , "chips" , "icecream"];
+
+
+// var uInput = prompt("Enter the product of the array. 1.choclates , 2. bantees , 3. chips , 4.icecream" )
+
+
+// var removedItem =  products.splice(uInput, 3);
+
+// console.log("'Removed Item:', removedItem");
+// console.log("The remaining items are: ")
+
+// for(i=0 ; i<products.length; i++){
+//    console.log(i + ": " + products[i0])
+//    console.log("The total remaining are: " + products.length);
+// }
+
+
+
+
+// task7:
+
+//  var uNationality = prompt("Enter Your nationality");
+//  var uGender = prompt("Enter Your gender");
+//  var uAge = +prompt("Enter Your age");
+
+//   if(nationality == 'Pakistani' || nationality == 'china' && age >18 || gender == male && age>=18 ){
+//      console.log("you are eligable for vote")
+//   }
+ 
+// else if(
+//    gender== female  && age >18){
+//       var marriage= prompt("your maritial status. 1. Married 2.UnMarried");
+//       if(marriage== Married){
+//          alert("You are eligible for vote");
+//       }
+
+//       }
+//       else {
+//          alert("you are not eligible for vote");
+//       }
+
+
+   
+// TASK 8:
+// var team = ["inzimam" , "afridi" , "saeed anwaar" , "waqar" , "shadab" , "babar"];
+// var final =[];
+// var final = team.slice(0, 6 );
+// console.log("finalist list");
+// for (var i = 0 ;i < final.length; i++)
+// {
+//    console.log(i + 1 + " : " + final[i])
+// }
